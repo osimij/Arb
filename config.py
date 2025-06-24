d04cd9f81c628879f45cc8b6b396429333a9e819
Jasur <osimijasur@gmail.com>
2025-06-24 20:11:10 +0500

    Update ADMIN_IDS in config.py with new admin user ID

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ TOKEN = os.environ.get("BOT_TOKEN", "7312413389:AAH1djA4FKjIGJwXMWmyOHORT5qckScq
 
 # A list of admin user IDs that can be changed directly in the code.
 ADMIN_IDS = [
-    5916988111,  # The test ID you wanted to add
+    6965346393,  # The test ID you wanted to add
     # You can add more admin IDs here, e.g., 123456789
 ]
 
