3409324d8dfbea06bb746138a7303f5d873d7cf6
Jasur <osimijasur@gmail.com>
2025-06-24 19:03:24 +0500

    feat: Update admin ID for testing

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 import os
 
 TOKEN = os.environ.get("BOT_TOKEN", "7312413389:AAH1djA4FKjIGJwXMWmyOHORT5qckScq52U")
-ADMIN_ID = int(os.environ.get("ADMIN_ID", "6965346393")) 
+ADMIN_ID = int(os.environ.get("ADMIN_ID", "5916988111")) 
