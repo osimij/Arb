7397784967102164920433277299b4016d3a15f5
Jasur O. <osimijasur@gmail.com>
2025-06-24 16:00:20 +0500

    Set up new Admin

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 import os
 
 TOKEN = os.environ.get("BOT_TOKEN", "7312413389:AAH1djA4FKjIGJwXMWmyOHORT5qckScq52U")
-ADMIN_ID = int(os.environ.get("ADMIN_ID", "788357726")) 
+ADMIN_ID = int(os.environ.get("ADMIN_ID", "6965346393")) 
